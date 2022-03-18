98964298f48f94462b45e3cb12d6bdc6bbaa5fe4
Elias <eliasventura23@gmail.com>
2022-03-17 23:18:50 -0400

    commit tree 24ce742f1bb415151cc3eba5ef96e4b9ff381074

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  //fetch("http://localhost:3000/dogs", configurationObject)
+    //.then(function (response) {
+      //return response.json();
+    //})
+    //.then(function (object) {
+      //console.log(object);
+    //})
+    //.catch(function (error) {
+        //alert("Bad things! Ragnarők!");
+        //console.log(error.message);
+      //});
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`, 
+            email: `${email}`
+        })
+    })
+    .then(resp => resp.json())
+    .then(function (object) {
+        document.body.append(object["id"])
+    })
+    .catch(function (error) {
+        document.body.append(error.message)
+    })
+    
+}
+
